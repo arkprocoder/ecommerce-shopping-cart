5839d843a70d805f39c8997a326471d9c785abe8
anees <aneesurrehman423@gmail.com>
2022-11-30 20:14:50 +0530

    added the signup part

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
-    return render(request,"authentication/signup.html")
+    if request.method=="POST":
+        email=request.POST['email']
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        if password!=confirm_password:
+            return HttpResponse("password incorrect") 
+            # return render(request,'auth/signup.html')                   
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse("email already exist")
+                # return render(request,'auth/signup.html')
+        except Exception as identifier:
+            pass
+        user = User.objects.create_user(email,email,password)
+        user.save()
+        return HttpResponse("User created",email)
+    return render(request,"signup.html")
+
+
+
+
 
 def handlelogin(request):
-    return render(request,"authentication/login.html")
+    return render(request,"login.html")
 
 
 def handlelogout(request):
-    return redirect('/authcart/login')
+    return redirect('/auth/login')
