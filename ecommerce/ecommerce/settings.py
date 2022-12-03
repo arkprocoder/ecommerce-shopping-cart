269aa33ff659cfb7c0ffd6b78adb0a7962077ff9
anees <aneesurrehman423@gmail.com>
2022-12-03 20:14:31 +0530

    added signup

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # sending emails
 
 EMAIL_HOST='smtpout.secureserver.net'
-EMAIL_HOST_USER='hr@airobotica.in'
-EMAIL_HOST_PASSWORD='Airobo@78678'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
